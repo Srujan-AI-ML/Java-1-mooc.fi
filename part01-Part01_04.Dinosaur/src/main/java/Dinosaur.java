2df01814889ea8a2a265ee73ce287cd8049eacad
Srujan-AI-ML <asgaladechronicles@gmail.com>
2026-06-16 16:07:14 +0530

    Add completed Part 1 Java exercises

---
+++ part01-Part01_04.Dinosaur/src/main/java/Dinosaur.java
@@ -0,0 +1,11 @@
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");  // Write your program here
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    
+    
+    }
+}
