f2175573d46498ba82c11e467fa93a5866264967
Srujan-AI-ML <asgaladechronicles@gmail.com>
2026-07-24 21:35:06 +0530

    my learning - personal project

---
+++ mylearning/src/part02/part02.java
@@ -0,0 +1,15 @@
+package part02;
+
+public class part02 {
+    public static void main(String[] args) {
+        int tulos = 0;
+
+        int i = 0;
+        while (i > 4) {
+            tulos += 3;
+            i++;
+        }
+
+        System.out.println(tulos);
+    }
+}
