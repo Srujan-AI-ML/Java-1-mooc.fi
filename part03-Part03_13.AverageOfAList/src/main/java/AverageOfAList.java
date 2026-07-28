9f101f50ff963976f917a41461f21764527d37ee
Srujan-AI-ML <asgaladechronicles@gmail.com>
2026-07-28 21:51:15 +0530

    part 3 java 1 and college works

---
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+            list.add(number);
+        }
+        int sum = 0;
+        for (int numbers : list) {
+            sum += numbers;
+        }
+        int size = list.size();
+        System.out.println("Average: " + 1.0 * sum / size);
+    }
+}
