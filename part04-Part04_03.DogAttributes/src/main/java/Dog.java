8b83c5265c41faada6468215f42358ac90f027a6
Srujan-AI-ML <asgaladechronicles@gmail.com>
2026-08-11 16:13:13 +0530

    part 4 java 1

---
+++ part04-Part04_03.DogAttributes/src/main/java/Dog.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asgal
+ */
+public class Dog {
+
+    private String name;
+    private int age;
+    private String breed;
+
+}
